baa5b2907f7f0fd6b2547752b230a7dd74d5f17c
TuPhung87 <tu.pt@ltv.dev>
2024-09-05 11:05:47 +0300

    complete hellospringboot

--- codes/model-view/bookstore/src/main/java/com/model/bookstore/BookstoreApplication.java
+++ codes/model-view/bookstore/src/main/java/com/model/bookstore/BookstoreApplication.java
@@ -1,13 +1,13 @@
-package com.model.assignment;
+package com.model.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssignmentApplication {
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignmentApplication.class, args);
+		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 }
