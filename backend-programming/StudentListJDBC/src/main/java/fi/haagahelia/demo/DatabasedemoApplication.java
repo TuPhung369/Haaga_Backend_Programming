27f577d9fca1635e69f1d66591e479bed19d4511
TuPhung87 <tu.pt@ltv.dev>
2024-09-12 15:33:34 +0300

    fix Student JDBC

--- backend-programming/StudentListJDBC/src/main/java/fi/haagahelia/demo/DatabasedemoApplication.java
+++ backend-programming/StudentListJDBC/src/main/java/fi/haagahelia/demo/DatabasedemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import fi.haagahelia.demo.domain.Student;
+//import fi.haagahelia.demo.domain.Student;
 import fi.haagahelia.demo.domain.StudentDAOImpl;
 
 @SpringBootApplication
@@ -17,17 +17,17 @@ public class DatabasedemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DatabasedemoApplication.class, args);
 	}
-	   
+
 	@Bean
 	public CommandLineRunner demo(StudentDAOImpl studentDAO) {
 		return (args) -> {
 			// Database is created by using resources/schema.sql
-			
+
 			// Insert some demo data
-	        studentDAO.save(new Student("John", "West"));
-	        studentDAO.save(new Student("Mike", "Mars"));
-	        studentDAO.save(new Student("Kate", "Johnson"));
-	       
+			// studentDAO.save(new Student("John", "West"));
+			// studentDAO.save(new Student("Mike", "Mars"));
+			// studentDAO.save(new Student("Kate", "Johnson"));
+
 		};
-	}	
+	}
 }
--- backend-programming/StudentListJDBC/src/main/java/fi/haagahelia/demo/domain/StudentRowMapper.java
+++ backend-programming/StudentListJDBC/src/main/java/fi/haagahelia/demo/domain/StudentRowMapper.java
@@ -4,15 +4,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.lang.NonNull;
 
 public class StudentRowMapper implements RowMapper<Student> {
 
-	public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+	@Override
+	public Student mapRow(@NonNull ResultSet rs, int rowNum) throws SQLException {
 		Student student = new Student();
 		student.setFirstName(rs.getString("first_name"));
 		student.setLastName(rs.getString("last_name"));
 		student.setId(rs.getLong("student_id"));
-		
+
 		return student;
 	}
 
