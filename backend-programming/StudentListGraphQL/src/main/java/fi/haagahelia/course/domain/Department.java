648dc7cbbe4a0823525537b41619ba2d617f5b52
TuPhung87 <tu.pt@ltv.dev>
2024-09-18 06:32:50 +0300

    sync

--- backend-programming/StudentListGraphQL/src/main/java/fi/haagahelia/course/domain/Department.java
+++ backend-programming/StudentListGraphQL/src/main/java/fi/haagahelia/course/domain/Department.java
@@ -1,12 +1,12 @@
 package fi.haagahelia.course.domain;
 
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 
 @Entity
 public class Department {
