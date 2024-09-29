de857fd4b5bd4116a1b8bb180c6db81b2946dfe5
TuPhung87 <tu.pt@ltv.dev>
2024-09-29 07:45:28 +0300

    sync

--- README.md
+++ README.md
@@ -13,3 +13,21 @@ enter password
 
 run mySQL with Docker
 `docker run --name mysql-8.0.39 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:8.0.39-debian`
+
+## Start the project
+
+Step 1: Run Docker
+Step 2: Run Dbeaver
+Step 3: Run application
+
+## kill the port 8080
+
+`netstat -ano | findstr :8080` -> cmd
+
+Use netstat to Look for Conflicts
+If you suspect that a third-party tool or service is reserving the port but can’t identify it, you can try using netstat with elevated privileges again:
+
+Run this command to get a complete overview of all listening ports and the associated processes:
+
+`netstat -ab`
+This will list all active connections and the process names, which can help you see if something is using port 8080.
---
+++ codes/bookstorerest/src/main/java/com/bookstorerest/authen/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.bookstorerest.authen;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+@Bean
+public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+ http
+ .authorizeHttpRequests()
+ .requestMatchers("/", "/home").permitAll()
+ .anyRequest().authenticated()
+ .and()
+ .formLogin()
+ .loginPage("/login")
+ .defaultSuccessUrl("/booklist”, true)
+ .permitAll() .and()
+ .logout()
+ .permitAll()
+ .and()
+ .httpBasic();
+ return http.build();
+ }
+}
