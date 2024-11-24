a2d1bde8b20577dbc946ef62b5339784dc7637e5
TuPhung87 <tu.pt@ltv.dev>
2024-11-24 18:01:38 +0200

    done

--- study/react-study/src/components/OAuth2RedirectHandler.js
+++ study/react-study/src/components/OAuth2RedirectHandler.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../GlobalContext";
 
 const OAuth2RedirectHandler = () => {
-  const { loginSocial, setLoginSocial } = useContext(GlobalContext);
+  const { setLoginSocial } = useContext(GlobalContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -12,23 +12,17 @@ const OAuth2RedirectHandler = () => {
 
     if (token) {
       console.log("Token received:", token);
-
       // Store the token in local storage
       localStorage.setItem("token", token);
       localStorage.setItem("isAuthenticated", "true");
       setLoginSocial(true);
-      console.log("Token stored in local storage", loginSocial);
       // Navigate to the home page
-      setTimeout(() => {
-        window.location.href = "http://localhost:3000";
-      }, 5000);
-      // Navigate to the home page
-      console.log("Navigating to home...");
+      window.location.href = "http://localhost:3000";
     } else {
       console.error("No token found in the URL");
       navigate("/login");
     }
-  }, [navigate, loginSocial, setLoginSocial]);
+  }, [navigate, setLoginSocial]);
 
   return <div>Processing authentication...</div>; // Loading indicator
 };
