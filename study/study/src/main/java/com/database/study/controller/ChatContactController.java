d816cc739776234e98b900b11271fa46e1abbcc2
TuPhung369 <bhm352@myy.haaga-helia.fi>
2025-04-16 19:03:55 +0300

    fix warnings for mapper.

--- study/study/src/main/java/com/database/study/controller/ChatContactController.java
+++ study/study/src/main/java/com/database/study/controller/ChatContactController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-import com.database.study.dto.request.ChatMessageRequest;
 import com.database.study.dto.response.ChatContactResponse;
 import com.database.study.dto.response.ChatMessageResponse;
 import com.database.study.dto.response.ChatMessageResponse.UserInfo;
